8d3620ef5d8a461eabcb26ac32e1a8339b369581
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-09 15:50:13 +0530

    solution of question 8

---
+++ question8.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 09 13:20:48 2017
+
+@author: User
+"""
+# for integer values '+' operator will add two variables  
+x =10
+y =20
+ans = x+y
+print type(x),type(y)
+print ans
+
+
+# for string  '+' operator will concatinate two variables  
+ x ='pritam'
+y ='sonawane'
+ans = x+y
+print type(x),type(y)
+print ans
