33886771d60ff13900ec483f8f89db5be726ca6f
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-09 14:24:01 +0530

    solution of question 2

---
+++ question2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 09 11:01:56 2017
+
+@author: User
+"""
+
+#pattern Program
+
+y =65
+for i in range(1, 5):
+    for j in range(1, i+1):
+        print chr(y),# converting integer to character value
+        y +=1
+    print '\r'
+    
+    
