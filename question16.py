d00bb57bcb896e831c40e302f5e25f0e80bfe55e
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-09 19:56:11 +0530

    solution of question 16

---
+++ question16.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 09 18:56:05 2017
+
+@author: User
+"""
+
+x = open("pks.txt",'r')
+i=0# counter initilize to zero
+for line in x: # iterate line by line
+    print line
+    i=i+1 
+  
+
+print i
