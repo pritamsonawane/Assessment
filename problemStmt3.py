af36c5b7fd34aa46a4bd522d60c4c7e9effce11c
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-10 12:44:35 +0530

    solution of problem statement 3

---
+++ problemStmt3.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 11:55:17 2017
+
+@author: User
+"""
+
+# pattern Print Program
+for i in range(1, 5):
+    for j in range(1, i+1):
+        print '*',
+    print '\r'
+for x in range(4,1,-1):
+    for y in range(1,x):
+        print '*',
+    print '\r'
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
