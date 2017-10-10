68a51a5ce5edd601347f5746bb5795959d70c1d2
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-10 16:38:58 +0530

    solution of problem statement 15

---
+++ problemStmt15.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 15:35:28 2017
+
+@author: User
+"""
+
+head= 35
+legs=94
+chLegs=2
+rabitLegs=4
+
+rabits=legs/2-head
+chicken=head - rabits
+
+
+print "We have %d rabits and %d chicken" %(rabits,chicken)
