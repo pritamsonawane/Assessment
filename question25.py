396fa706862dc0d5503716f65478d0bd0ece8dbb
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-10 11:29:29 +0530

    solution of question25

---
+++ question25.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 10:59:56 2017
+
+@author: User
+"""
+
+
+
+def function():
+    dic={}
+    i=input("enter the number: ")
+    dic= {n: n**2 for n in range(i)}
+    print dic
+    
