b00789976e9dab02c9df8b9a5f93942d5988025a
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-10 12:44:26 +0530

    solution of problem statement 2

---
+++ problemStmt2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 11:44:11 2017
+
+@author: User
+"""
+
+
+temp=float(input("enter the temperature: "))
+Fahrenheit = 1.8*temp + 32
+print "Fahrenheit temperature is %f" % (Fahrenheit) 
