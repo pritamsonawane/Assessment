348401eac3447e639227570054faceeee0443396
pritamsonawane <pritam.sonawane@quantiphi.com>
2017-10-10 12:44:42 +0530

    solution of problem statement 4

---
+++ problemStmt4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 12:35:15 2017
+
+@author: User
+"""
+
+#Fibonacci series between 0 to 50
+a,b = 0,1
+print "Fibonacci series between 0 to 50: "
+while a < 50:
+    print (a),
+    a,b = b,a+b
